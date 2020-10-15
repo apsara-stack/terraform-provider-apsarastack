c888b45628e20227b58adc28e979170423921e44
click2cloud-omega <click2cloud-omega@click2cloud.net>
2020-10-15 17:03:18 +0530

    Update error file for MQ

--- apsarastack/errors.go
+++ apsarastack/errors.go
@@ -29,6 +29,7 @@ const (
 	RamInstanceNotFound        = "Forbidden.InstanceNotFound"
 	ApsaraStackGoClientFailure = "ApsaraStackGoClientFailure"
 	DenverdinoApsaraStackgo    = ErrorSource("[SDK denverdino/aliyungo ERROR]")
+	ThrottlingUser             = "Throttling.User"
 )
 
 var SlbIsBusy = []string{"SystemBusy", "OperationBusy", "ServiceIsStopping", "BackendServer.configuring", "ServiceIsConfiguring"}
