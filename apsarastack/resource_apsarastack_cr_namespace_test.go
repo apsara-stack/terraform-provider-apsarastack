003344575a28c078f0e76a6099ea7a5d99528461
click2cloud-omega <click2cloud-omega@click2cloud.net>
2020-10-15 12:52:39 +0530

    Added Resource,Datasource and document for container registry

---
+++ apsarastack/resource_apsarastack_cr_namespace_test.go
@@ -0,0 +1,234 @@
+package apsarastack
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/cr"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+
+	"github.com/aliyun/terraform-provider-apsarastack/apsarastack/connectivity"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func init() {
+	resource.AddTestSweepers("apsarastack_cr_namespace", &resource.Sweeper{
+		Name: "apsarastack_cr_namespace",
+		F:    testSweepCRNamespace,
+	})
+}
+
+func testSweepCRNamespace(region string) error {
+	// skip not supported region
+	for _, r := range connectivity.CRNoSupportedRegions {
+		if region == string(r) {
+			log.Printf("[INFO] testSweepCRNamespace skipped not supported region: %s", region)
+			return nil
+		}
+	}
+
+	rawClient, err := sharedClientForRegion(region)
+	if err != nil {
+		return WrapError(fmt.Errorf("error getting ApsaraStack client: %s", err))
+	}
+	client := rawClient.(*connectivity.ApsaraStackClient)
+
+	prefixes := []string{
+		"tf-testAcc",
+		"tf_testAcc",
+	}
+
+	req := cr.CreateGetNamespaceListRequest()
+
+	raw, err := client.WithCrClient(func(crClient *cr.Client) (interface{}, error) {
+		return crClient.GetNamespaceList(req)
+	})
+
+	if err != nil {
+		log.Printf("[ERROR] %s ", WrapErrorf(err, DefaultErrorMsg, "apsarastack_cr_namespace", req.GetActionName(), ApsaraStackSdkGoERROR))
+		return nil
+	}
+
+	var resp crDescribeNamespaceListResponse
+	err = json.Unmarshal(raw.(*cr.GetNamespaceListResponse).GetHttpContentBytes(), &resp)
+	if err != nil {
+		log.Printf("[ERROR] %s", WrapError(err))
+		return nil
+	}
+
+	var ns []string
+	for _, n := range resp.Data.Namespace {
+		for _, p := range prefixes {
+			if strings.HasPrefix(n.Namespace, strings.ToLower(p)) {
+				ns = append(ns, n.Namespace)
+			}
+		}
+	}
+
+	for _, n := range ns {
+		err := resource.Retry(5*time.Minute, func() *resource.RetryError {
+			req := cr.CreateDeleteNamespaceRequest()
+			req.Namespace = n
+
+			_, err := client.WithCrClient(func(crClient *cr.Client) (interface{}, error) {
+				return crClient.DeleteNamespace(req)
+			})
+			if err != nil {
+				if IsExpectedErrors(err, []string{"NAMESPACE_NOT_EXIST"}) {
+					return nil
+				}
+				return resource.RetryableError(WrapErrorf(err, DefaultErrorMsg, n, req.GetActionName(), ApsaraStackSdkGoERROR))
+			}
+
+			crService := CrService{client}
+
+			_, err = crService.DescribeCrNamespace(n)
+			if err != nil {
+				if NotFoundError(err) {
+					return nil
+				}
+				return resource.RetryableError(WrapErrorf(err, DefaultErrorMsg, n, req.GetActionName(), ApsaraStackSdkGoERROR))
+			}
+
+			time.Sleep(15 * time.Second)
+			return resource.RetryableError(WrapError(Error("DeleteNamespace timeout")))
+		})
+		if err != nil {
+			log.Printf("[ERROR] Failed to delete Namespace: %s", n)
+		}
+	}
+	return nil
+}
+
+func TestAccApsaraStackCRNamespace_Basic(t *testing.T) {
+	var v *cr.GetNamespaceResponse
+	resourceId := "apsarastack_cr_namespace.default"
+	ra := resourceAttrInit(resourceId, crNamespaceMap)
+	serviceFunc := func() interface{} {
+		return &CrService{testAccProvider.Meta().(*connectivity.ApsaraStackClient)}
+	}
+	rc := resourceCheckInit(resourceId, &v, serviceFunc)
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(1000000, 9999999)
+	name := fmt.Sprintf("tf-testacc-cr-ns-%d", rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, resourceCRNamespaceConfigDependence)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+			testAccPreCheckWithRegions(t, false, connectivity.CRNoSupportedRegions)
+		},
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"name":               name,
+					"auto_create":        "false",
+					"default_visibility": "PUBLIC",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"name":               name,
+						"auto_create":        "false",
+						"default_visibility": "PUBLIC",
+					}),
+				),
+			},
+			{
+				ResourceName:      resourceId,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"auto_create": "true",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"auto_create": "true",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"default_visibility": "PRIVATE",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"default_visibility": "PRIVATE",
+					}),
+				),
+			},
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"name":               name,
+					"auto_create":        "false",
+					"default_visibility": "PUBLIC",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(map[string]string{
+						"name":               name,
+						"auto_create":        "false",
+						"default_visibility": "PUBLIC",
+					}),
+				),
+			},
+		},
+	})
+}
+
+func TestAccApsaraStackCRNamespace_Multi(t *testing.T) {
+	var v *cr.GetNamespaceResponse
+	resourceId := "apsarastack_cr_namespace.default.4"
+	ra := resourceAttrInit(resourceId, crNamespaceMap)
+	serviceFunc := func() interface{} {
+		return &CrService{testAccProvider.Meta().(*connectivity.ApsaraStackClient)}
+	}
+	rc := resourceCheckInit(resourceId, &v, serviceFunc)
+	rac := resourceAttrCheckInit(rc, ra)
+	testAccCheck := rac.resourceAttrMapUpdateSet()
+	rand := acctest.RandIntRange(1000000, 9999999)
+	name := fmt.Sprintf("tf-testacc-cr-ns-%d", rand)
+	testAccConfig := resourceTestAccConfigFunc(resourceId, name, resourceCRNamespaceConfigDependence)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+			testAccPreCheckWithRegions(t, false, connectivity.CRNoSupportedRegions)
+		},
+		IDRefreshName: resourceId,
+		Providers:     testAccProviders,
+		CheckDestroy:  rac.checkResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccConfig(map[string]interface{}{
+					"name":               name + "${count.index}",
+					"auto_create":        "false",
+					"default_visibility": "PUBLIC",
+					"count":              "5",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheck(nil),
+				),
+			},
+		},
+	})
+}
+
+func resourceCRNamespaceConfigDependence(name string) string {
+	return ""
+}
+
+var crNamespaceMap = map[string]string{
+	"name":               CHECKSET,
+	"auto_create":        CHECKSET,
+	"default_visibility": CHECKSET,
+}
