aa976b473815215e0391d4dc4c0d5ee6706d50cc
Click2cloud-Gamma <click2cloud_gamma@click2cloud.net>
2020-11-25 10:30:29 +0530

    bug fixes

--- apsarastack/resource_apsarastack_instance.go
+++ apsarastack/resource_apsarastack_instance.go
@@ -30,8 +30,8 @@ func resourceApsaraStackInstance() *schema.Resource {
 		},
 
 		Timeouts: &schema.ResourceTimeout{
-			Create: schema.DefaultTimeout(10 * time.Minute),
-			Update: schema.DefaultTimeout(10 * time.Minute),
+			Create: schema.DefaultTimeout(20 * time.Minute),
+			Update: schema.DefaultTimeout(20 * time.Minute),
 			Delete: schema.DefaultTimeout(20 * time.Minute),
 		},
 
