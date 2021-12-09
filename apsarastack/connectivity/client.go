c865726dd43e80f7c7861814ba02cdf32e5838f8
click2cloud-lamda <click2cloud-lamda@click2cloud.net>
2021-12-09 14:36:47 +0530

    Updated Provider Version

--- apsarastack/connectivity/client.go
+++ apsarastack/connectivity/client.go
@@ -111,7 +111,7 @@ type ApiVersion string
 
 // The main version number that is being run at the moment.
 
-var ProviderVersion = "1.0.9"
+var ProviderVersion = "1.0.10"
 var TerraformVersion = strings.TrimSuffix(schema.Provider{}.TerraformVersion, "-dev")
 var goSdkMutex = sync.RWMutex{} // The Go SDK is not thread-safe
 
