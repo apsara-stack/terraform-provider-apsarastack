09fa4de2b6e8bf77e2659ba9dc4be319399b76c3
zyj01365919 <2149047846@qq.com>
2022-04-07 11:09:51 +0800

    测试

--- apsarastack/data_source_apsarastack_ascm_quotas_test.go
+++ apsarastack/data_source_apsarastack_ascm_quotas_test.go
@@ -31,6 +31,7 @@ func TestAccApsaraStackAscm_quotasDataSource(t *testing.T) {
 
 const dataSourceApsaraStackAscm_Quota = `
 
+
 data "apsarastack_ascm_quotas" "default" {
   quota_type = "organization"
   quota_type_id = 1
