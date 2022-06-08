b2ff676e8bca6a54ea5f13f222c3eb458b45908b
chenchi <cc01032862@alibaba-inc.com>
2022-06-08 17:12:22 +0800

    add dms

--- apsarastack/connectivity/client.go
+++ apsarastack/connectivity/client.go
@@ -1324,3 +1324,28 @@ func (client *ApsaraStackClient) NewRosClient() (*rpc.Client, error) {
 	}
 	return conn, nil
 }
+
+func (client *ApsaraStackClient) NewDmsenterpriseClient() (*rpc.Client, error) {
+	productCode := "dmsenterprise"
+	endpoint := client.Config.DmsEnterpriseEndpoint
+	if v, ok := client.Config.Endpoints[productCode]; !ok || v.(string) == "" {
+		if err := client.loadEndpoint(productCode); err != nil {
+			endpoint = "dms-enterprise.aliyuncs.com"
+			client.Config.Endpoints[productCode] = endpoint
+			log.Printf("[ERROR] loading %s endpoint got an error: %#v. Using the central endpoint %s instead.", productCode, err, endpoint)
+		}
+	}
+	if v, ok := client.Config.Endpoints[productCode]; ok && v.(string) != "" {
+		endpoint = v.(string)
+	}
+	if endpoint == "" {
+		return nil, fmt.Errorf("[ERROR] missing the product %s endpoint.", productCode)
+	}
+	sdkConfig := client.teaSdkConfig
+	sdkConfig.SetEndpoint(endpoint)
+	conn, err := rpc.NewClient(&sdkConfig)
+	if err != nil {
+		return nil, fmt.Errorf("unable to initialize the %s client: %#v", productCode, err)
+	}
+	return conn, nil
+}
