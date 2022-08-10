a024108c1556f2ca190b7399af391a8c904f016c
chenchi <cc01032862@alibaba-inc.com>
2022-08-10 17:01:15 +0800

    fix image import

--- apsarastack/resource_apsarastack_image_import.go
+++ apsarastack/resource_apsarastack_image_import.go
@@ -176,7 +176,7 @@ func resourceApsaraStackImageImportCreate(d *schema.ResourceData, meta interface
 	addDebug(request.GetActionName(), raw, request.RpcRequest, request)
 	resp, _ := raw.(*ecs.ImportImageResponse)
 	d.SetId(resp.ImageId)
-	stateConf := BuildStateConf([]string{"Waiting"}, []string{"Available"}, d.Timeout(schema.TimeoutCreate), 1*time.Minute, ecsService.ImageStateRefreshFunc(d.Id(), []string{"CreateFailed", "UnAvailable"}))
+	stateConf := BuildStateConf([]string{"Waiting"}, []string{"Available"}, d.Timeout(schema.TimeoutCreate), 3*time.Hour, ecsService.ImageStateRefreshFunc(d.Id(), []string{"CreateFailed", "UnAvailable"}))
 	if _, err := stateConf.WaitForState(); err != nil {
 		return WrapErrorf(err, IdMsg, d.Id())
 	}
