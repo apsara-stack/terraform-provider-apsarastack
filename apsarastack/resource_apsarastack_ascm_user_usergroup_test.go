40dfd48bd9331da9b090a183fa0b86ab53ddaa25
chenchi <cc01032862@alibaba-inc.com>
2023-04-20 15:27:32 +0800

    fix user to usergroup

--- apsarastack/resource_apsarastack_ascm_user_usergroup_test.go
+++ apsarastack/resource_apsarastack_ascm_user_usergroup_test.go
@@ -3,7 +3,6 @@ package apsarastack
 import (
 	"fmt"
 	"github.com/apsara-stack/terraform-provider-apsarastack/apsarastack/connectivity"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 	"testing"
@@ -16,8 +15,6 @@ func TestAccApsaraStackAscm_UserGroup_User_Basic(t *testing.T) {
 	serviceFunc := func() interface{} {
 		return &AscmService{testAccProvider.Meta().(*connectivity.ApsaraStackClient)}
 	}
-	rand := acctest.RandInt()
-	name := fmt.Sprintf("tf-ascmusergroup%v", rand)
 	rc := resourceCheckInit(resourceId, &v, serviceFunc)
 	rac := resourceAttrCheckInit(rc, ra)
 	testAccCheck := rac.resourceAttrMapUpdateSet()
@@ -33,7 +30,7 @@ func TestAccApsaraStackAscm_UserGroup_User_Basic(t *testing.T) {
 		CheckDestroy: testAccCheckAscmUserGroupUserDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: fmt.Sprintf(testAccCheckAscmUserGroupUserRoleBinding, name, rand),
+				Config: fmt.Sprintf(testAccCheckAscmUserGroupUserRoleBinding),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheck(nil),
 				),
@@ -67,31 +64,12 @@ func testAccCheckAscmUserGroupUserDestroy(s *terraform.State) error {
 }
 
 const testAccCheckAscmUserGroupUserRoleBinding = `
-resource "apsarastack_ascm_organization" "default" {
- name = "Test_binder"
- parent_id = "1"
-}
-
-resource "apsarastack_ascm_user_group" "default" {
- group_name =      "%s"
- organization_id = apsarastack_ascm_organization.default.org_id
-}
-
-resource "apsarastack_ascm_user" "default" {
- cellphone_number = "13900000000"
- email = "test@gmail.com"
- display_name = "C2C-DELTA"
- organization_id = apsarastack_ascm_organization.default.org_id
- mobile_nation_code = "91"
- login_name = "User_Role_Test%d"
- login_policy_id = 1
-}
 
 resource "apsarastack_ascm_usergroup_user" "default" {
   //login_name = apsarastack_ascm_user.default.login_name
-  login_names = ["User_Role_Test6304175127373178963", "User_Role_Test7233024715252325400"]
+  login_names = ["dsfasdsfaa"]
   //login_names = ["[\"User_Role_Test929636066677054911\"]"]
-  user_group_id = apsarastack_ascm_user_group.default.user_group_id
+  user_group_id = "82"
 }
 `
 
